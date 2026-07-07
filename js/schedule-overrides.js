edda0808a429ca1438e9d955b3c7541ea2449ed5
dntjd0601-1 <dntjd0601-1@users.noreply.github.com>
2026-07-07 12:41:10 +0000

    deploy: publish site update 8cd9be1630169e77c3624fa22ab27d731392e87a

---
+++ js/schedule-overrides.js
@@ -0,0 +1,13 @@
+﻿/** Manual schedule overrides from schedule-overrides.json */
+(function () {
+  if (typeof SCHEDULE_EVENTS === 'undefined') return;
+  const overrides = {
+  "2026-07-03": [{ "type": "live", "title": "소통방송" }],
+  "2026-07-05": [{ "type": "live", "title": "휴방" }],
+  "2026-07-06": [{ "type": "live", "title": "휴방" }],
+  "2026-07-07": [{ "type": "live", "title": "소통방송" }]
+};
+  Object.keys(overrides).forEach((date) => {
+    SCHEDULE_EVENTS[date] = overrides[date];
+  });
+})();
